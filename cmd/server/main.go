5af62cd42753797779c82ed2feebcf9427d0cf58
KayOI <kay@onlineimage.com>
2026-08-21 10:14:46 -0600

    basic json publishing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os/signal"
 	"log"
 	
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -17,6 +19,25 @@ func main() {
 	}
 	defer connection.Close()
 	fmt.Println("Connection successful.")
+
+	ch, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("Error establishing channel: %s", err)
+	}
+	defer ch.Close()
+	fmt.Printf("error: %s", err)
+
+	playState := routing.PlayingState{
+		IsPaused: true,
+	}
+	fmt.Println("------ATTEMPTING TO PUBLISH------")
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, playState)
+	if err != nil {
+		log.Fatalf("Error publishing JSON: %s", err)
+	}
+	fmt.Printf("------PUBLISH SUCCESSFUL------ error: %s", err)
+
+	// accept CTRL+C to end program
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
