5bb804d5aad9d9c723c35241289249af399541da
KayOI <kay@onlineimage.com>
2026-08-21 09:20:37 -0600

    establish connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"log"
+	
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	cUrl := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(cUrl)
+	if err != nil {
+		log.Fatalf("Error connecting to server: %s", err)
+	}
+	defer connection.Close()
+	fmt.Println("Connection successful.")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Program shutting down.")
 }
